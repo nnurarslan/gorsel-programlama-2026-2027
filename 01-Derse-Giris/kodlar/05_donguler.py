ac3c790ea50d15b223885a2fc401c4a898e1560f
NUR ARSLAN <115628373+nnurarslan@users.noreply.github.com>
2026-09-21 13:42:28 +0300

    Döngüler örneği eklendi

---
+++ 01-Derse-Giris/kodlar/05_donguler.py
@@ -0,0 +1,20 @@
+# for ve while döngüleri
+
+print("1) for döngüsü")
+for sayi in range(1, 6):
+    print("Sayı:", sayi)
+
+print("\n2) Liste üzerinde döngü")
+dersler = ["Python", "OOP", "Qt Designer", "PyQt"]
+
+for ders in dersler:
+    print(ders)
+
+print("\n3) while döngüsü")
+sayac = 3
+
+while sayac > 0:
+    print(sayac)
+    sayac -= 1
+
+print("Başlıyoruz!")
