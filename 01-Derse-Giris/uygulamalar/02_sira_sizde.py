e4ec4dfce88c0dab959c2ff400ab3c24c5d63f6c
NUR ARSLAN <115628373+nnurarslan@users.noreply.github.com>
2026-09-21 13:42:44 +0300

    Öğrenci uygulaması eklendi

---
+++ 01-Derse-Giris/uygulamalar/02_sira_sizde.py
@@ -0,0 +1,16 @@
+# SIRA SİZDE
+#
+# Bir "Vücut Kitle İndeksi Hesaplama" programı yazın.
+#
+# Program:
+# 1. Kullanıcıdan adını almalı.
+# 2. Kilosunu kg olarak almalı.
+# 3. Boyunu metre olarak almalı.
+# 4. VKİ = kilo / boy**2 formülüyle hesaplamalı.
+# 5. Sonucu ekrana yazdırmalı.
+#
+# Bonus:
+# Hesaplama işlemini bir fonksiyon içerisinde yapın.
+
+# Kodunuzu aşağıya yazın:
+
